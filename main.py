12b208ce3ab825340176c425a6e42c1ecfd515f3
karthikp-1036 <karthikprasad2206@gmail.com>
2025-07-09 20:12:01 +0530

    added CORS

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--5173--96435430.local-credentialless.webcontainer-api.io/"],  # Replace with your frontend domain in production
+    allow_origins=["*"],  # Replace with your frontend domain in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
